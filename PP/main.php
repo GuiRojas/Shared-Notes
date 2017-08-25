a4b97342529b2b0da868e3ef2c9e6ba16135c02a
Guilherme Rojas <30930231+nodoya@users.noreply.github.com>
2017-08-25 10:51:39 -0300

    criei samerda
    ha

---
+++ PP/main.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Site show de bola</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<?php
+	include("cabecalho.inc");
+	?>	
+
+
+
+	<div class="fundoDoSite">
+		<?php
+		include("rodape.inc");
+		?>			
+	</div>
+
+</body>
+</html>
