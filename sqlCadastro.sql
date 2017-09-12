7ed9f38e2fec6988bc59777d69c5ec4660c639f1
franciscomaiaN <frn640@gmail.com>
2017-09-12 10:02:55 -0300

    sql
    tem as tabelas do sql agr

---
+++ sqlCadastro.sql
@@ -0,0 +1,28 @@
+create table usuario(
+	username varchar(15) primary key,
+	email varchar(100) not null,
+	nome varchar(50) not null,
+	senha varchar(20) not null
+)
+
+create table categoria(
+	codCategoria int identity(1,1) primary key,
+	tipo varchar(20) not null,
+	linguagem varchar(10) not null
+)
+
+create table pagina(
+	url varchar(100) primary key,
+	titulo varchar(30) not null,
+	descricao ntext,
+	categoria int not null,
+	constraint fkCategoria foreign key (categoria) references categoria (codCategoria)
+)
+
+create table comentario(
+	codComentario int identity(1,1) primary key,
+	url varchar(100) not null,
+	username varchar(15) not null,
+	constraint fkURL foreign key (url) references pagina (url),
+	constraint fkUsername foreign key (username) references usuario (username)
+)
