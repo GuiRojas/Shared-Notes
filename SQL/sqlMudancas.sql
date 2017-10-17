020c67d817a1c2fe30745db0d84a6e4982cdc299
franciscomaiaN <frn640@gmail.com>
2017-10-17 11:23:11 -0200

    ajeitei oq o visual pediu

--- SQL/sqlMudancas.sql
+++ SQL/sqlMudancas.sql
@@ -7,3 +7,6 @@ alter table usuario add especialidade varchar(15)
 update usuario set user_status = 'allahu a kibar' where username = 'JohnnyKaparrala'
 
 select * from usuario
+
+alter table usuario alter column user_status ntext
+alter table usuario alter column especialidade ntext
