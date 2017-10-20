8b4138398ec4c57de069052f55f4fdc19d0cf90b
nodoya <guiribrojas@gmail.com>
2017-10-20 11:16:17 -0200

    n precsa fazer upload em tudo

--- Perguntas/prg_new.php
+++ Perguntas/prg_new.php
@@ -5,6 +5,17 @@
 	<link rel="stylesheet" type="text/css" href="../CSS/padraoSite.css">
 	<script type="text/javascript" src="../JS/jquery-3.2.1.js"></script>
 	<script type="text/javascript" src="../JS/script.js"></script>
+	<style type="text/css">
+		#perg
+		{
+		height:200px;
+		width: 500px;
+		}
+		#formPerg
+		{
+		margin-left: 20px;
+		}
+	</style>
 </head>
 <body>
 	<?php
@@ -15,7 +26,14 @@
 		
 	<div id="container">
 		
-
+		<form method="POST" action="new.php" id="formPerg">
+			Título:<br><input type="text" name="titulo" maxlength="100"><br>
+			Categoria:<input type="text" name="cat"><br><br>
+			Pergunta:<br><input type="text" name="perg" id="perg"><br>
+			<br><br>
+			<input type="submit" name="Logar" id="enviar">	
+						
+		</form>
 
 	</div>
 	
