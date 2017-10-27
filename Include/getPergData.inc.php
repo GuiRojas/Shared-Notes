c69de056093c90f49a50203ecb554f662fe44846
franciscomaiaN <frn640@gmail.com>
2017-10-27 08:21:03 -0200

    to arrumando

--- Include/getPergData.inc.php
+++ Include/getPergData.inc.php
@@ -1,11 +1,11 @@
 <?php 
 	include 'connect.inc.php';
 	if ( $conexao){
-		$consultaUsuario = sqlsrv_query($conexao,"SELECT titulo as titulo,texto as texto,categoria as cat,criador as criador FROM pergunta WHERE titulo = '$nomePerg'");
+		$consultaUsuario = sqlsrv_query($conexao,"SELECT titulo, texto, categoria, criador FROM pergunta WHERE titulo = '$nomePerg'");
 		while ( $linha = sqlsrv_fetch_array( $consultaUsuario, SQLSRV_FETCH_ASSOC)){
 			$titulo = $linha['titulo'];
 			$texto = $linha['texto'];
-			$cat = $linha['cat'];
+			$cat = $linha['categoria'];
 			$criador = $linha['criador'];
 		}
 	}
