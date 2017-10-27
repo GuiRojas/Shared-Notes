77b43b9612e2515595bee87d840a593efdf18c7c
franciscomaiaN <frn640@gmail.com>
2017-10-27 11:07:27 -0200

    to fazendo as perguntas

--- Include/getPergData.inc.php
+++ Include/getPergData.inc.php
@@ -1,9 +1,9 @@
 <?php 
 	include 'connect.inc.php';
 	if ( $conexao){
-		$consultaUsuario = sqlsrv_query($conexao,"SELECT titulo, texto, categoria, criador FROM pergunta WHERE titulo = '$nomePerg'");
+		$consultaUsuario = sqlsrv_query($conexao,"SELECT titulo, texto, categoria, criador FROM pergunta WHERE titulo = '$titulo'");
 		while ( $linha = sqlsrv_fetch_array( $consultaUsuario, SQLSRV_FETCH_ASSOC)){
-			$titulo = $linha['titulo'];
+			$manchete = $linha['titulo'];
 			$texto = $linha['texto'];
 			$cat = $linha['categoria'];
 			$criador = $linha['criador'];
--- Perguntas/perg.php
+++ Perguntas/perg.php
@@ -3,7 +3,7 @@
 
 	<link rel="shortcut icon" href="../Imagens/logoSite.png" />
 	<title><?php echo "Perfis"; ?></title>
-	<link rel="stylesheet" type="text/css" href="../CSS/padraoSite.css">
+	<link rel="stylesheet" type="text/css" href="../CSS/procuraPerfil.css">
 	<script type="text/javascript" src="../JS/jquery-3.2.1.js"></script>
 	<script type="text/javascript" src="../JS/script.js"></script>
 </head>
@@ -11,23 +11,34 @@
 	<?php
 		if ( isset($_GET['query'])){ // inicio do if
 			$titulo = $_GET['query'];
+			include '../Include/getPergData.inc.php';
 			include '../Include/top.inc.php';
 			include '../Include/side.inc.php';
+			
+	?>
+		<div id="container">
+			<h3><i><?php echo "$cat"; ?></i></h3> <br>
 
-			$nomePerg = $_GET['query'];
-			include '../Include/getPergData.inc.php';
-		}
+			<img src='$urlFoto' class='img'>
+			<div id="pergunta">
+			<?php
+				echo "<form align = 'center'> $manchete </form><br> $texto";
+
+			?>
+				
+			</div>
+
+
+
+
+		</div>
+	<?php
+		} //fim do if
 		else {
 			$titulo = '';
 			include '../Include/top.inc.php';
 			include '../Include/side.inc.php';
 		}
-	?> 
-	<div id="container">
-
-	</div>
-	
-	<?php
 		include '../Include/bot.inc.php';
 	?>
 </body>
