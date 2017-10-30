51f2be38763ae7b50610a11e3ce3003618ae56ad
Johnny Kaparrala <joao.lucas.c.alvarez@gmail.com>
2017-10-30 11:12:39 -0200

    asas

--- Include/getPergData.inc.php
+++ Include/getPergData.inc.php
@@ -1,17 +1,13 @@
 <?php 
 	include 'connect.inc.php';
 	if ( $conexao){
-		$consultaUsuario = sqlsrv_query($conexao,"SELECT titulo, texto, categoria, criador FROM pergunta WHERE titulo = '$titulo'");
+		$consultaUsuario = sqlsrv_query($conexao,"SELECT titulo, texto, categoria, criador, data FROM pergunta WHERE titulo = '$titulo'");
 		while ( $linha = sqlsrv_fetch_array( $consultaUsuario, SQLSRV_FETCH_ASSOC)){
 			$manchete = $linha['titulo'];
 			$texto = $linha['texto'];
 			$cat = $linha['categoria'];
 			$criador = $linha['criador'];
-			$dia = $linha[''];
-			$mes = $linha[''];
-			$ano = $linha[''];
-			$hora = $linha[''];
-			$minutos = $linha[''];
+
 		}
 	}
 ?>
