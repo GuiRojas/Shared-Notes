08562224d66749e9286a4106d81e36b9575e7802
franciscomaiaN <frn640@gmail.com>
2017-11-07 09:16:56 -0200

    ta legal mas precisa melhorar

--- Include/getPergData.inc.php
+++ Include/getPergData.inc.php
@@ -1,13 +1,17 @@
 <?php 
 	include 'connect.inc.php';
 	if ( $conexao){
-		$consultaUsuario = sqlsrv_query($conexao,"SELECT titulo, texto, categoria, criador, data FROM pergunta WHERE titulo = '$titulo'");
-		while ( $linha = sqlsrv_fetch_array( $consultaUsuario, SQLSRV_FETCH_ASSOC)){
+		$consultaPergunta = sqlsrv_query($conexao,"SELECT titulo, texto, categoria, criador, data FROM pergunta WHERE titulo = '$titulo'");
+		while ( $linha = sqlsrv_fetch_array( $consultaPergunta, SQLSRV_FETCH_ASSOC)){
 			$manchete = $linha['titulo'];
 			$texto = $linha['texto'];
 			$cat = $linha['categoria'];
 			$criador = $linha['criador'];
-
 		}
+
+		$consultaUsuario = sqlsrv_query($conexao,"SELECT foto FROM usuario WHERE username = '$criador'");
+
+		while ( $linha = sqlsrv_fetch_array( $consultaUsuario, SQLSRV_FETCH_ASSOC))
+			$urlFoto = $linha['foto'];
 	}
 ?>
