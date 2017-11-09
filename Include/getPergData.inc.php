7c206b884978881802999ffdab1f199d64153a2c
nodoya <guiribrojas@gmail.com>
2017-11-09 07:39:46 -0200

    iniciei coment,
    preparei so os arquivos basicos

--- Include/getPergData.inc.php
+++ Include/getPergData.inc.php
@@ -1,8 +1,9 @@
 <?php 
 	include 'connect.inc.php';
 	if ( $conexao){
-		$consultaPergunta = sqlsrv_query($conexao,"SELECT titulo, texto, categoria, criador, data FROM pergunta WHERE titulo = '$titulo'");
+		$consultaPergunta = sqlsrv_query($conexao,"SELECT codPergunta,titulo, texto, categoria, criador, data FROM pergunta WHERE titulo = '$titulo'");
 		while ( $linha = sqlsrv_fetch_array( $consultaPergunta, SQLSRV_FETCH_ASSOC)){
+			$codPergunta = $linha['codPergunta'];
 			$manchete = $linha['titulo'];
 			$texto = $linha['texto'];
 			$cat = $linha['categoria'];
