892957f9abfde5553ce8ef74ad858f4b2f828bf7
nodoya <guiribrojas@gmail.com>
2017-11-10 07:49:37 -0200

    q????

--- SQL/sqlCadastro.sql
+++ SQL/sqlCadastro.sql
@@ -24,6 +24,23 @@ create table comentario(
 	constraint fkURL foreign key (codPergunta) references pergunta (codPergunta),
 	constraint fkUsername foreign key (username) references usuario (username)
 )
+/*
+create table seguir(
+	codSeguidor int identity(1,1) primary key,
+	uSeguidor varchar(25) not null,
+	uSeguindo varchar(25) not null,
+	constraint fkUsgr foreign key (uSeguidor) references usuario(username),
+	constraint fkUsgn foreign key (uSeguindo) references usuario(username)	
+)
+*/
+create table projeto(
+	titulo varchar(50) primary key,
+	descricao ntext not null,
+	nota ntext not null,
+	criador varchar(25) not null,
+	constraint fkCri foreign key(criador) references usuario(username)
+)
+
 
 create table projComentario(
 	codProjComentario int identity(1,1) primary key,
@@ -34,22 +51,6 @@ create table projComentario(
 	data datetime not null
 	constraint fkPrj foreign key (Projeto) references projeto (titulo),
 	constraint fkCrid foreign key (criador) references usuario (username)
-)
-
-create table seguir(
-	codSeguidor int identity(1,1) primary key,
-	uSeguidor varchar(25) not null,
-	uSeguindo varchar(25) not null,
-	constraint fkUsgr foreign key (uSeguidor) references usuario(username),
-	constraint fkUsgn foreign key (uSeguindo) references usuario(username)	
-)
-
-create table projeto(
-	titulo varchar(50) primary key,
-	descricao ntext not null,
-	nota ntext not null,
-	criador varchar(25) not null,
-	constraint fkCri foreign key(criador) references usuario(username)
 )
 
 select * from comentario
