659f9eafb544dac5bbd1c2cdf5133a31889da73f
franciscomaiaN <frn640@gmail.com>
2017-11-17 08:22:28 -0200

    to ajeitando

--- Perguntas/perg_pesq.php
+++ Perguntas/perg_pesq.php
@@ -1,7 +1,7 @@
 <html>
 <head>
 	<link rel="shortcut icon" href="../Imagens/logoSite.png" />
-	<title>Perguntas</title>
+	<title>Pesquisar Perguntas</title>
 	<link rel="stylesheet" type="text/css" href="../CSS/procuraPerfil.css">
 	<script type="text/javascript" src="../JS/jquery-3.2.1.js"></script>
 	<script type="text/javascript" src="../JS/script.js"></script>
--- Projetos/projNew.php
+++ Projetos/projNew.php
@@ -1,7 +1,7 @@
 <html>
 <head>
 	<link rel="shortcut icon" href="../Imagens/logoSite.png" />
-	<title><?php echo "Projeto"; ?></title>
+	<title>Postar Projeto</title>
 	<link rel="stylesheet" type="text/css" href="../CSS/procuraPerfil.css">
 	<script type="text/javascript" src="../JS/jquery-3.2.1.js"></script>
 	<script type="text/javascript" src="../JS/script.js"></script>
