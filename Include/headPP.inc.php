025aa8ac14c142210e9eccfe49deab27c7da7886
franciscomaiaN <frn640@gmail.com>
2017-11-27 09:51:58 -0200

    opa

--- Include/headPP.inc.php
+++ Include/headPP.inc.php
@@ -1,6 +1,6 @@
 <head>
 	<link rel="shortcut icon" href="../Imagens/logoSite.png" />
-	<title>$title</title>
+	<title><?php $title ?></title>
 	<link rel="stylesheet" type="text/css" href="../CSS/procuraPerfil.css">
 	<script type="text/javascript" src="../JS/jquery-3.2.1.js"></script>
 	<script type="text/javascript" src="../JS/script.js"></script>	
